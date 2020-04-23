8234238215a9214d143fef1e4fddeb9211b2195f
ahyanhuan <ahyanhuan@sina.com>
2020-04-23 23:37:44 +0800

    first time commit

---
+++ read-tomcat/src/main/java/com/yh/tomcat/read/chapter2/HelloWorldServlet.java
@@ -0,0 +1,46 @@
+package com.yh.tomcat.read.chapter2;
+
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * 原生Servlet
+ *
+ * @author yanhuan
+ */
+public class HelloWorldServlet implements Servlet {
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        System.out.println("init");
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("service");
+        PrintWriter writer = servletResponse.getWriter();
+        writer.write("Hello, servlet");
+        writer.println("Goodbye, servlet");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy");
+    }
+}
